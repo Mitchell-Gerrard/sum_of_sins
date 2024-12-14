5cb5f051eacf67cb2cbd59c7a0ed90acf4793041
Mitchell-Gerrard <mitchell.gerrard99@hotmail.com>
2024-12-14 16:37:22 +0000

    Update sum_of_sins visualization to handle varying powers and save plots

--- sum_of_sins.py
+++ sum_of_sins.py
@@ -8,22 +8,32 @@ def sum_of_sins(n):
     return x, sum_sins,result_an
 
 if __name__ == "__main__":
-    n = 10**8
+    power=8
+    n = 10**power
     results_reds = []
+    #x = np.memmap('x.dat', dtype='float64', mode='w+', shape=(n,))
+    #result_sum = np.memmap('result_sum.dat', dtype='float64', mode='w+', shape=(n,))
+    #result_an = np.memmap('result_an.dat', dtype='float64', mode='w+', shape=(n,))
 
+    x[:], result_sum[:], result_an[:] = sum_of_sins(n)
+    mean_residual = result_sum - result_an
     x,result_sum,result_an = sum_of_sins(n)
     mean_residual = result_sum - result_an
 
 
     # Calculate statistics
 
+    #coeffs = np.polyfit(x, mean_residual, 10)
+    #linear_fit = np.polyval(coeffs, x)
+    for i in range(power):
+        plt.plot(x, mean_residual, label='Mean Residual')
 
-    plt.plot(x, mean_residual, label='Mean Residual')
-
-
-    plt.xlabel('n')
-    plt.xscale('log')
-    plt.yscale('symlog', linthresh=1e-16)
-    plt.ylabel('Residual')
-    plt.legend()
-    plt.show()
+        #plt.plot(x, linear_fit, label='Linear Fit', linestyle='--')
+        plt.xlabel('n')
+        plt.xscale('log')
+        plt.yscale('symlog', linthresh=1e-16)
+        plt.xlim(10**i, 10**(i+1))
+        plt.ylabel('Residual')
+        plt.legend()
+        plt.savefig(f'sum_of_sins_{power}.png')
+        plt.clf()
